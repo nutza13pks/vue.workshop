f9c7706f94dd408e8c083ce499621806349f2f1d
nutza13pks <nutza13pks@hotmail.com>
2021-03-21 14:48:38 +0700

    Set up a structure
    - set up vuetify
    - set up router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,58 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import Stock from "@/views/Stock.vue";
+import StockCreate from "@/views/StockCreate.vue";
+import StockEdit from "@/views/StockEdit.vue";
+import Report from "@/views/Report.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/login",
+    name: "login",
+    component: Login,
   },
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    name: "about",
+    component: About,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/stock",
+    name: "stock",
+    component: Stock,
+  },
+  {
+    path: "/stock-create",
+    name: "stock-create",
+    component: StockCreate,
+  },
+  {
+    path: "/stock-edit/:id",
+    name: "stock-edit",
+    component: StockEdit,
+  },
+  {
+    path: "/report",
+    name: "report",
+    component: Report,
+  },
+  {
+    path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "*",
+    redirect: "/login" // page not found go to login page
   },
 ];
 
