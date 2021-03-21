b29de93db67cf4025adb5fb3ed73096d621bb019
nutza13pks <nutza13pks@hotmail.com>
2021-03-21 16:47:43 +0700

    Set up a structure
    - set up header
    - set up menu

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -1,13 +1,29 @@
 <template>
-  <h1>Header</h1>
+  <div>
+    <v-app-bar app color="primary" dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>CMStock Workshop V{{ version }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <span>Username</span>
+      <v-btn icon @click="onClickLogOff">
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "header",
+  computed: {
+    version() {
+      return "1.0"
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -1,13 +1,38 @@
 <template>
-  <h1>Menu</h1>
+  <v-navigation-drawer app permanent>
+    <router-link to="/" exact>
+      <v-img src="@/assets/vue_display.jpg" alt="" width="100%" />
+    </router-link>
+
+    <v-list shaped>
+      <v-list-item-group v-model="selectedMenu" mandatory color="primary">
+        <v-list-item v-for="([icon, text, route], index) in menus" :key="index">
+           <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title >{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+       selectedMenu:0,
+        menus: [
+        ['mdi-apps-box', 'Stock', '/stock'],
+        ['mdi-chart-areaspline', 'Report', '/report'],
+        ['mdi-text-box-check-outline', 'About', '/about'],
+      ]
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
