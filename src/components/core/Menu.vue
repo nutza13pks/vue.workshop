248e83f027c8e2776120a029cb0a416599d5e352
nutza13pks <nutza13pks@hotmail.com>
2021-03-24 21:39:22 +0700

    Vue Chart JS
    - New function for view chart and random data for test chart

--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer app permanent>
+  <v-navigation-drawer app permanent absolute dark src="@/assets/background_menu.jpg">
     <router-link to="/" exact>
       <v-img src="@/assets/vue_display.jpg" alt="" width="100%" />
     </router-link>
@@ -7,6 +7,7 @@
     <v-list shaped>
       <v-list-item-group v-model="selectedMenu" mandatory color="primary">
         <v-list-item
+          class="tile"
           v-for="([icon, text, route], index) in menus"
           :key="index"
           @click="onClickMenu(route)"
@@ -55,4 +56,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.v-list-item-group .v-list-item--active {
+  color: #fff !important;
+  font-weight: bold;
+}
+.theme--dark.v-list-item:not(.v-list-item--active):not(.v-list-item--disabled) {
+  opacity: 0.7;
+}
+.tile {
+  color: white;
+}
+.tile:hover {
+  background: green;
+}
+.tile:active {
+  background: #05ab71;
+}
+</style>
--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,13 +1,79 @@
-<template>
-  <h1>Report</h1>
+<template lang="html">
+  <v-container>
+    <v-card>
+      <v-btn class="success" @click="fillData()">Refresh</v-btn>
+      <LineChart style="height: 300px" :chartData="datacollection" />
+      <BarChart style="height: 300px" :chartData="datacollection" />
+    </v-card>
+  </v-container>
 </template>
 
-<script>
-export default {
+<script lang="js">
+import LineChart from "@/components/charts/LineChart.vue";
+import BarChart from "@/components/charts/BarChart.vue";
 
+  export default  {
+    name: 'report',
+      components: {
+      LineChart,
+      BarChart
+    },
+    mounted () {
+        this.fillData()
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    methods: {
+     fillData () {
+      this.datacollection = {
+          //Data to be represented on x-axis
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          datasets: [
+            {
+              label: '2019',
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#249EBF',
+              data: this.getRandomInt(),
+
+              borderColor: [
+                '#43A047'
+               ],
+               borderWidth: 2,
+               backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            }
+          ]};
+
+      },
+      getRandomInt () {
+        let randoms = []
+        for (let index = 0; index < 12; index++) {
+          randoms.push(Math.floor(Math.random() * (50000 - 5 + 1)) + 5)
+        }
+        return randoms
+      }
+    },
+    computed: {
+
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
 </style>
